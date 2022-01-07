672613aa9a227450dd38f17a997705fcb0b6160b
Schaeferjo <mail@joschaefer.com>
2022-01-07 17:35:46 +0100

    feat: add responsive slideshow

--- src/routes/bilder.svelte
+++ src/routes/bilder.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { base } from '$app/paths';
-	import '../app.css';
+	import Slideshow from '$lib/slideshow/Slideshow.svelte';
 </script>
 
 <div class="hero h-50v">
@@ -11,13 +11,7 @@
 </div>
 
 <div class="mx-14 lg:mx-16">
-	<div
-		class="main max-w-screen-lg mx-auto -mt-12 bg-white px-6 lg:px-12 lg:px-20 py-12 lg:py-16 relative z-50"
-	>
-		<h1 class="text-no_brown">Bilder</h1>
-
-		<h2 class="font-semibold">slfngkdfnnsdfnsdfkj</h2>
-		<h2 class="font-bold">slfngkdfnnsdfnsdfkj</h2>
-		<p>Lorem ipsum</p>
+	<div class="main">
+		<Slideshow />
 	</div>
 </div>
