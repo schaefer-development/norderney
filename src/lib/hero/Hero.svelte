d2b70c3ae63917c11a37cc6c8db69c5638dc5a9d
Schaeferjo <mail@joschaefer.com>
2022-01-27 21:33:46 +0100

    feat: add border-wave

--- src/lib/hero/Hero.svelte
+++ src/lib/hero/Hero.svelte
@@ -7,4 +7,6 @@
 		class="bg_layer w-full hero_height bg-cover bg-no_bg_hero "
 		style="background-image: url({base}/hero_placeholder.jpg)"
 	/>
+
+	<div class="w-full h-16 -mt-16 bg-cover" style="background-image: url({base}/wave_border.svg)" />
 </div>
