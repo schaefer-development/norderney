5f10d56981f3bf959b4f446d5c2606f5c62d9a6d
Robert Schäfer <git@roschaefer.de>
2022-01-28 21:45:21 +0100

    feat: custom arrows

--- src/lib/slideshow/Slideshow.svelte
+++ src/lib/slideshow/Slideshow.svelte
@@ -17,14 +17,13 @@
 			gap: '1rem',
 			cover: true,
 			pagination: false,
-			classes: {
-				arrows: 'splide__arrows your-class-arrows',
-				arrow: 'splide__arrow your-class-arrow',
-				prev: 'splide__arrow--prev your-class-prev',
-				next: 'splide__arrow--next your-class-next'
-			}
 		}}
 	>
+
+  <div class="splide__arrows" slot="before-track">
+		<button class="splide__arrow splide__arrow--prev" type="button" aria-controls="splide01-track" aria-label="Go to last slide"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 40" width="40" height="40"><path fill="red" d="m15.5 0.932-4.3 4.38 14.5 14.6-14.5 14.5 4.3 4.4 14.6-14.6 4.4-4.3-4.4-4.4-14.6-14.6z"></path></svg></button><button class="splide__arrow splide__arrow--next" type="button" aria-controls="splide01-track" aria-label="Next slide"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 40" width="40" height="40"><path d="m15.5 0.932-4.3 4.38 14.5 14.6-14.5 14.5 4.3 4.4 14.6-14.6 4.4-4.3-4.4-4.4-14.6-14.6z"></path></svg></button>
+	</div>
+
 		<SplideSlide>
 			<img src="{base}/aus_wohnung02.jpg" alt="Ferienwohnung Am Meer Norderneys" />
 		</SplideSlide>
@@ -77,11 +76,4 @@
 	.push_up_footer {
 		margin-bottom: -23vh;
 	}
-
-	/* purgecss start ignore */
-	.your-class-next {
-		border: 1px solid red !important;
-	}
-
-	/* purgecss end ignore */
 </style>
