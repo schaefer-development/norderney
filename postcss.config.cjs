85e6a8a19cad47ab5117e0a4196bb1a51dbe28a5
Robert Schäfer <git@roschaefer.de>
2022-01-29 15:22:38 +0100

    feat: customize splide.js with tailwind classes

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,9 +1,11 @@
 const tailwindcss = require('tailwindcss');
 const autoprefixer = require('autoprefixer');
+const postcssImport = require('postcss-import');
 
 const config = {
 	plugins: [
 		//Some plugins, like tailwindcss/nesting, need to run before Tailwind,
+		postcssImport,
 		tailwindcss(),
 		//But others, like autoprefixer, need to run after,
 		autoprefixer
