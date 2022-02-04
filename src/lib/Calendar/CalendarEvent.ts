ab53ca914c15175a618fdf023dca72c05f1b1186
Robert Schäfer <git@roschaefer.de>
2022-02-04 20:15:18 +0100

    fix: ugly as hell solution for intersecting events

--- src/lib/Calendar/CalendarEvent.ts
+++ src/lib/Calendar/CalendarEvent.ts
@@ -5,6 +5,7 @@ export default class CalendarEvent {
 	constructor(event: { start: { date: string }; end: { date: string } }) {
 		this.start = new Date(event.start.date);
 		this.end = new Date(event.end.date);
+		this.end.setDate(this.end.getDate() - 1)
 	}
 
 	startsAt(date: Date): boolean {
